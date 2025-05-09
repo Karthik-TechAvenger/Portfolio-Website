41147b0b58bc85fede00610df601fef120c78631
Karthik Kulkarni <karthikrk2004@gmail.com>
2025-05-10 00:00:08 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import LogoSection from "./components/LogoSection.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <ShowcaseSection />
       <NavBar />
       <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   );
 }
